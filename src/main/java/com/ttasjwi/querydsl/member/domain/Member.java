30f32ad4e858209eb16bd9ce8d8eee8c9687ec1d
ttasjwi <ttasjwi920@gmail.com>
2022-05-31 20:55:33 +0900

    기본 엔티티 생성 - Member, Team

---
+++ src/main/java/com/ttasjwi/querydsl/member/domain/Member.java
@@ -0,0 +1,48 @@
+package com.ttasjwi.querydsl.member.domain;
+
+import com.ttasjwi.querydsl.team.domain.Team;
+import lombok.*;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name", "age"})
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String name) {
+        this(name,0);
+    }
+
+    public Member(String name, int age) {
+        this(name, age, null);
+    }
+
+    public Member(String name, int age, Team team) {
+        this.name = name;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
---
+++ src/main/java/com/ttasjwi/querydsl/team/domain/Team.java
@@ -0,0 +1,28 @@
+package com.ttasjwi.querydsl.team.domain;
+
+import com.ttasjwi.querydsl.member.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private final List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+}
