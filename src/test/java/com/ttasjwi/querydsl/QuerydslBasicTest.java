1196bfdbd6e741ea10febe322281ed696e13cdc8
ttasjwi <ttasjwi920@gmail.com>
2022-06-04 17:49:53 +0900

    Querydsl을 통한 엔티티 질의
    - 사전작업 : compileQuerydsl을 통해 Q엔티티 생성
    - JpaQueryFactory 생성
    - queryFactory를 통해 자바 코드로 쿼리

---
+++ src/test/java/com/ttasjwi/querydsl/QuerydslBasicTest.java
@@ -0,0 +1,75 @@
+package com.ttasjwi.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ttasjwi.querydsl.member.domain.Member;
+import com.ttasjwi.querydsl.member.domain.QMember;
+import com.ttasjwi.querydsl.team.domain.Team;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class QuerydslBasicTest {
+
+    @Autowired
+    private EntityManager em;
+
+    private JPAQueryFactory queryFactory;
+
+    @BeforeEach
+    void before() {
+        queryFactory = new JPAQueryFactory(em);
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+    }
+
+    @Test
+    public void startJPQL() {
+        //JPQL로 쿼리 작성
+        String query =
+                "SELECT m " +
+                "From Member as m " +
+                "Where m.name = :name";
+
+        Member findMember = em.createQuery(query, Member.class)
+                .setParameter("name", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getName()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl() {
+        QMember m = new QMember("m");
+
+        Member findMember = queryFactory
+                .select(m)
+                .from(m)
+                .where(m.name.eq("member1"))
+                .fetchOne();
+
+        assertThat(findMember.getName()).isEqualTo("member1");
+    }
+
+}
